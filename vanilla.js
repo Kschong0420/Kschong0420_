031694110a87bfbfe9c15507c97503e64f190eff
Kschong0420 <79582179+Kschong0420@users.noreply.github.com>
2021-02-25 14:39:21 +0800

    Add files via upload

---
+++ vanilla.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'vanilla',
+    description: "Vanilla self introduction video!",
+    execute(message, args){
+        message.channel.send('https://youtu.be/ZmIqFqnMj60');
+    }
+}
